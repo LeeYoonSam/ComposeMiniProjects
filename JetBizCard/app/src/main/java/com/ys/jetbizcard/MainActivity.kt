64d94d20db096a57a7328cf4f0767489315d7e4c
YS <yoonsam86@gmail.com>
2023-04-28 16:52:09 +0900

    JetBizCard - Card 추가 및 코너 사이즈, elevation 조정

--- JetBizCard/app/src/main/java/com/ys/jetbizcard/MainActivity.kt
+++ JetBizCard/app/src/main/java/com/ys/jetbizcard/MainActivity.kt
@@ -5,12 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ys.jetbizcard.ui.theme.JetBizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,7 +43,17 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CreateBizCard() {
     Surface(modifier = Modifier.fillMaxSize()) {
-        
+        Card(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 4.dp
+            )
+        ) {
+
+        }
     }
 }
 
