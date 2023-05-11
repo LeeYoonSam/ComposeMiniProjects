1534fba6ef1c5e825a4f6e3c82d2966ffcd3e232
YS <yoonsam86@gmail.com>
2023-05-11 17:54:19 +0900

    JetTipApp - InputField 컴포넌트 추가 kotlin, compose 디펜던시 버전 호환성 처리

--- JetTipApp/app/src/main/java/com/ys/jettipapp/MainActivity.kt
+++ JetTipApp/app/src/main/java/com/ys/jettipapp/MainActivity.kt
@@ -6,24 +6,29 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.ys.jettipapp.components.InputField
 import com.ys.jettipapp.ui.theme.JetTipAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -80,8 +85,19 @@ fun TopHeader(totalPerPerson: Double = 134.0) {
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun MainContent() {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -94,9 +110,18 @@ fun MainContent() {
                 .fillMaxWidth()
                 .padding(8.dp)
         ) {
-            Text(text = "Hello World")
-            Text(text = "Hello World")
-            Text(text = "Hello World")
+            InputField(
+                valueState = totalBillState,
+                labelId = "Enter Bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+                    // Todo - onValueChanged
+
+                    keyboardController?.hide()
+                }
+            )
         }
     }
 }
