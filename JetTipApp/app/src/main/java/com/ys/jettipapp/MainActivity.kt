7b6a09450363ca21f2bc216bf71b37dab36005d2
YS <yoonsam86@gmail.com>
2023-05-21 21:09:52 +0900

    JetTipApp - Add tip percentage for slider

--- JetTipApp/app/src/main/java/com/ys/jettipapp/MainActivity.kt
+++ JetTipApp/app/src/main/java/com/ys/jettipapp/MainActivity.kt
@@ -160,13 +160,11 @@ fun BillForm(
 @Composable
 fun SplitUnit() {
 
-    val splitNumber = remember {
-        mutableStateOf(1)
-    }
-
     val sliderPositionState = remember {
         mutableStateOf(0f)
     }
+
+    val tipPercentage = (sliderPositionState.value * 100).toInt()
 
     val splitByState = remember {
         mutableStateOf(1)
@@ -245,7 +243,7 @@ fun SplitUnit() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "33%")
+        Text(text = "$tipPercentage %")
 
         Spacer(modifier = Modifier.height(14.dp))
 
---
+++ JetTipApp/app/src/main/java/com/ys/jettipapp/util/Util.kt
@@ -0,0 +1,2 @@
+package com.ys.jettipapp.util
+
