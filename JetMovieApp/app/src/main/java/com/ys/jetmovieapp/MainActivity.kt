c16d28862cbef689dc9c782ad2e7aa13a8182f50
YS <yoonsam86@gmail.com>
2023-05-29 13:08:51 +0900

    JetMovieApp - Add Movie List using LazyColumn

--- JetMovieApp/app/src/main/java/com/ys/jetmovieapp/MainActivity.kt
+++ JetMovieApp/app/src/main/java/com/ys/jetmovieapp/MainActivity.kt
@@ -3,16 +3,35 @@ package com.ys.jetmovieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ys.jetmovieapp.ui.theme.JetMovieAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,16 +58,63 @@ fun MyApp(content: @Composable () -> Unit) {
                     title = { Text(text = "Movies") }
                 )
             }
-        ) { paddingValues ->
-            content()
+        ) {
+            Column(modifier = Modifier.padding(it)) {
+                content()
+            }
         }
     }
 }
 
 @Composable
-fun MainContent() {
-    Surface(color = MaterialTheme.colorScheme.background) {
-        Text(text = "Hello")
+fun MainContent(
+    movieList: List<String> = listOf(
+        "Avatar",
+        "300",
+        "Harry Potter",
+        "Cats",
+        "범죄 도시1",
+        "범죄 도시2",
+        "범죄 도시3",
+        "Guardians of Galaxy",
+        "Life"
+    )
+) {
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn {
+            items(items = movieList) {
+                MovieRow(movie = it)
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieRow(movie: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(130.dp)
+            .padding(4.dp),
+        shape = RoundedCornerShape(CornerSize(16.dp)),
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 6.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RectangleShape,
+                shadowElevation = 4.dp
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+            }
+
+            Text(text = movie)
+        }
     }
 }
 
@@ -58,4 +124,10 @@ fun MyAppPreview() {
     MyApp {
         MainContent()
     }
+}
+
+@Preview
+@Composable
+private fun MovieRowPreview() {
+    MovieRow(movie = "Harry Potter")
 }
