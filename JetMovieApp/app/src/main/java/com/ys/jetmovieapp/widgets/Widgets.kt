2b4ecfffb94ceb5fa24b7f12a97d78226bdd2b46
YS <yoonsam86@gmail.com>
2023-06-17 17:59:33 +0900

    JetMovieApp - Using coil to add images

--- JetMovieApp/app/src/main/java/com/ys/jetmovieapp/widgets/Widgets.kt
+++ JetMovieApp/app/src/main/java/com/ys/jetmovieapp/widgets/Widgets.kt
@@ -2,26 +2,31 @@ package com.ys.jetmovieapp.widgets
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.ys.jetmovieapp.model.Movie
+import com.ys.jetmovieapp.model.getMovies
 
 @Composable
 fun MovieRow(
@@ -50,10 +55,34 @@ fun MovieRow(
                 shape = RectangleShape,
                 shadowElevation = 4.dp
             ) {
-                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+                AsyncImage(
+                    model = movie.images.first(),
+                    contentDescription = "Movie Poster",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.clip(CircleShape)
+                )
             }
 
-            Text(text = movie.title)
+            Column(modifier = Modifier.padding(4.dp)) {
+                Text(
+                    style = MaterialTheme.typography.headlineSmall,
+                    text = movie.title
+                )
+                Text(
+                    style = MaterialTheme.typography.bodySmall,
+                    text = "Director: ${movie.director}"
+                )
+                Text(
+                    style = MaterialTheme.typography.bodySmall,
+                    text = movie.year
+                )
+            }
         }
     }
+}
+
+@Preview
+@Composable
+fun MovieRowPreview() {
+    MovieRow(movie = getMovies().first())
 }
