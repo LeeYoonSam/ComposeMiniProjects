a0f8df66bb7764673d4c26a32484decbd1dab348
YS <yoonsam86@gmail.com>
2023-06-18 23:30:26 +0900

    Finishing up the details screen

--- JetMovieApp/app/src/main/java/com/ys/jetmovieapp/screens/details/DetailsScreen.kt
+++ JetMovieApp/app/src/main/java/com/ys/jetmovieapp/screens/details/DetailsScreen.kt
@@ -3,10 +3,19 @@ package com.ys.jetmovieapp.screens.details
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -19,11 +28,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.ys.jetmovieapp.model.getMovies
+import com.ys.jetmovieapp.widgets.MovieRow
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailScreen(navController: NavController, movieData: String?) {
+fun DetailScreen(navController: NavController, movieId: String?) {
+    val movie = getMovies().first { it.id == movieId }
 
     Scaffold(
         topBar = {
@@ -51,13 +70,47 @@ fun DetailScreen(navController: NavController, movieData: String?) {
         ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                verticalArrangement = Arrangement.Top
             ) {
+                Surface(modifier = Modifier.padding(12.dp)) {
+                    MovieRow(
+                        movie = movie
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+                Divider()
                 Text(
-                    text = movieData.toString(),
-                    style = MaterialTheme.typography.headlineMedium
+                    text = "Movie Images",
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontWeight = FontWeight.Bold
                 )
+                LazyRow {
+                    items(items = movie.images) { image ->
+                        MovieImage(image)
+                    }
+                }
             }
         }
+    }
+}
+
+@Composable
+private fun MovieImage(imageUrl: String) {
+    Card(
+        modifier = Modifier
+            .padding(12.dp)
+            .size(240.dp),
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 5.dp)
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(imageUrl)
+                .crossfade(true)
+                .build(),
+            contentDescription = "Movie Poster",
+            contentScale = ContentScale.Crop,
+            error = rememberVectorPainter(image = Icons.Default.Warning)
+        )
     }
 }
--- JetMovieApp/app/src/main/java/com/ys/jetmovieapp/widgets/Widgets.kt
+++ JetMovieApp/app/src/main/java/com/ys/jetmovieapp/widgets/Widgets.kt
@@ -19,7 +19,6 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -30,8 +29,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -40,11 +39,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.ys.jetmovieapp.model.Movie
 import com.ys.jetmovieapp.model.getMovies
 
 @Composable
 fun MovieRow(
+    modifier: Modifier = Modifier,
     movie: Movie,
     onItemClick: (String) -> Unit = {}
 ) {
@@ -55,7 +56,6 @@ fun MovieRow(
     Card(
         modifier = Modifier
             .fillMaxWidth()
-//            .height(130.dp)
             .padding(4.dp)
             .clickable {
                 onItemClick(movie.id)
@@ -67,20 +67,18 @@ fun MovieRow(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start
         ) {
-            Surface(
+            AsyncImage(
                 modifier = Modifier
+                    .size(100.dp)
                     .padding(12.dp)
-                    .size(100.dp),
-                shape = RectangleShape,
-                shadowElevation = 4.dp
-            ) {
-                AsyncImage(
-                    model = movie.images.first(),
-                    contentDescription = "Movie Poster",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier.clip(CircleShape)
-                )
-            }
+                    .clip(CircleShape),
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(movie.images.first())
+                    .crossfade(true)
+                    .build(),
+                contentDescription = "Movie Poster",
+                contentScale = ContentScale.Crop,
+            )
 
             Column(modifier = Modifier.padding(4.dp)) {
                 Text(
