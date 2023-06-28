3d2991ae846d9128ab6d797253556c5a2239fd0a
YS <yoonsam86@gmail.com>
2023-06-28 09:14:35 +0900

    JetNote - Adding Flow State to getNotes Functions

--- JetNote/app/src/main/java/com/ys/jetnote/data/NoteDatabaseDao.kt
+++ JetNote/app/src/main/java/com/ys/jetnote/data/NoteDatabaseDao.kt
@@ -7,11 +7,12 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.ys.jetnote.model.Note
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteDatabaseDao {
     @Query("SELECT * FROM notes_tbl")
-    suspend fun getNotes(): List<Note>
+    suspend fun getNotes(): Flow<List<Note>>
 
     @Query("SELECT * FROM notes_tbl WHERE id =:id")
     suspend fun getNoteById(id: String): Note
