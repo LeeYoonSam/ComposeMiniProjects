b111ea0ab94f17c99050eec1571073e9cab5f09d
YS <yoonsam86@gmail.com>
2023-07-05 08:08:59 +0900

    JetTrivia - Finishing up the Repository class

--- JetTrivia/app/src/main/java/com/ys/jettrivia/repository/QuestionRepository.kt
+++ JetTrivia/app/src/main/java/com/ys/jettrivia/repository/QuestionRepository.kt
@@ -1,5 +1,6 @@
 package com.ys.jettrivia.repository
 
+import android.util.Log
 import com.ys.jettrivia.data.DataOrException
 import com.ys.jettrivia.model.QuestionItem
 import com.ys.jettrivia.network.QuestionApi
@@ -8,5 +9,18 @@ import javax.inject.Inject
 class QuestionRepository @Inject constructor(
     private val api: QuestionApi
 ) {
-    private val listOfQuestions = DataOrException<ArrayList<QuestionItem>, Boolean, Exception>()
+    private val dataOrException = DataOrException<ArrayList<QuestionItem>, Boolean, Exception>()
+
+    suspend fun getAllQuestions(): DataOrException<ArrayList<QuestionItem>, Boolean, Exception> {
+        try {
+            dataOrException.loading = true
+            dataOrException.data = api.getAllQuestions()
+            if (dataOrException.data.toString().isNotEmpty()) dataOrException.loading = false
+        } catch (exception: Exception) {
+            dataOrException.e = exception
+            Log.d("Exc", "getAllQuestions: ${dataOrException.e!!.localizedMessage}")
+        }
+
+        return dataOrException
+    }
 }
