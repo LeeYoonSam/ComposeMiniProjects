1346e95ab2ef653384ebea09367ab13bdc605cc7
YS <yoonsam86@gmail.com>
2023-07-09 14:36:54 +0900

    JetTrivia - Create UI - Dotted Line Composable

--- JetTrivia/app/src/main/java/com/ys/jettrivia/component/Questions.kt
+++ JetTrivia/app/src/main/java/com/ys/jettrivia/component/Questions.kt
@@ -1,10 +1,12 @@
 package com.ys.jettrivia.component
 
 import android.util.Log
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
@@ -12,6 +14,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -53,7 +57,29 @@ fun QuestionDisplay() {
             horizontalAlignment = Alignment.Start
         ) {
             QuestionTracker()
+            DrawDottedLine()
         }
+    }
+}
+
+@Composable
+fun DrawDottedLine() {
+    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+    ) {
+        drawLine(
+            color = AppColors.mLightGray,
+            start = Offset.Zero,
+            end = Offset(
+                x = size.width,
+                y = 0f
+            ),
+            pathEffect = pathEffect
+        )
     }
 }
 
